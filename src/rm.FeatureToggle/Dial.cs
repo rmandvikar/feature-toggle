3ea7275b0a41187a3c1e76c518b680b577e0472a
hippy <hippy2981@gmail.com>
2021-05-01 16:48:40 -0700

    ft dial: Add impl

---
+++ src/rm.FeatureToggle/Dial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace rm.FeatureToggle
+{
+	/// <inheritdoc cref="IDial"/>
+	public class Dial : IDial
+	{
+		private readonly Random rng;
+
+		/// <param name="rng">
+		/// rng.
+		/// <para>Note: <paramref name="rng"/> needs to be thread-safe.</para>
+		/// </param>
+		public Dial(Random rng)
+		{
+			this.rng = rng
+				?? throw new ArgumentNullException(nameof(rng));
+		}
+
+		/// <inheritdoc/>
+		public bool ToDial(double percentage)
+		{
+			if (percentage <= 0)
+			{
+				return false;
+			}
+			if (percentage >= 100)
+			{
+				return true;
+			}
+
+			// .01-100.00, exclude 0
+			// in case of int percentage (0-100), it will be rng.Next(1, 101)
+			//	if -99, then percentage 99 will be 100%, so -100
+			//	if 0-, then percentage 1 will be 2 parts for 0, 1, so 1-
+			//	so 1-100, i.e. rng.Next(1-101)
+			var randomDialValue = (double)rng.Next(1, 100_01) / 100;
+			return randomDialValue <= percentage;
+		}
+	}
+}
---
+++ src/rm.FeatureToggle/IDial.cs
@@ -0,0 +1,14 @@
+﻿namespace rm.FeatureToggle
+{
+	/// <summary>
+	/// Defines dial methods.
+	/// </summary>
+	public interface IDial
+	{
+		/// <summary>
+		/// Returns true with a <paramref name="percentage"/> probability.
+		/// </summary>
+		/// <param name="percentage">Number [0.00, 100.00] with 2 decimal places precision.</param>
+		bool ToDial(double percentage);
+	}
+}
