8123adfe5b8f5412bedf90cc719e845ec2f9c429
hippy <hippy2981@gmail.com>
2021-05-02 15:14:03 -0700

    ft count then percentage dial: Add impl

---
+++ src/rm.FeatureToggle/CountThenPercentageDial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace rm.FeatureToggle
+{
+	/// <inheritdoc cref="ICountThenPercentageDial"/>
+	public class CountThenPercentageDial : ICountThenPercentageDial
+	{
+		private readonly ICountDial countDial;
+		private readonly IPercentageDial percentageDial;
+
+		public CountThenPercentageDial(ICountDial countDial, IPercentageDial percentageDial)
+		{
+			this.countDial = countDial
+				?? throw new ArgumentNullException(nameof(countDial));
+			this.percentageDial = percentageDial
+				?? throw new ArgumentNullException(nameof(percentageDial));
+		}
+
+		/// <inheritdoc/>
+		public bool ToDial(long count, double percentage)
+		{
+			return countDial.ToDial(count)
+				|| percentageDial.ToDial(percentage);
+		}
+	}
+}
