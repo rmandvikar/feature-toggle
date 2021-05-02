472f73a9ba3c4d1a334490b001eab4728bf376cb
hippy <hippy2981@gmail.com>
2021-05-02 15:14:03 -0700

    ft count then percentage dial: Add impl

---
+++ src/rm.FeatureToggle/ICountThenPercentageDial.cs
@@ -0,0 +1,14 @@
+﻿namespace rm.FeatureToggle
+{
+	/// <summary>
+	/// Defines dial methods with count and then percentage.
+	/// </summary>
+	public interface ICountThenPercentageDial
+	{
+		/// <summary>
+		/// Returns true for first <paramref name="count"/> calls, and then with a
+		/// <paramref name="percentage"/> probability.
+		/// </summary>
+		bool ToDial(long count, double percentage);
+	}
+}
---
+++ tests/rm.FeatureToggleTest/CountThenPercentageDialTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using rm.FeatureToggle;
+using rm.Random2;
+
+namespace rm.FeatureToggleTest
+{
+	[TestFixture]
+	public class CountThenPercentageDialTests
+	{
+		private static readonly Random rng = RandomFactory.GetThreadStaticRandom();
+		private const int iterations = 1_000_000;
+
+		[TestFixture]
+		public class ToDial
+		{
+			[Test]
+			[TestCase(0, 0d, false)]
+			[TestCase(0, 100d, true)]
+			public void Verify_ToDial_For_Edge_Values(long count, double percentage, bool toDial)
+			{
+				var dial = new CountThenPercentageDial(new CountDial(), new PercentageDial(rng));
+				Assert.AreEqual(toDial, dial.ToDial(count, percentage));
+			}
+
+			[Test]
+			[TestCase(1, 0d)]
+			public void Verify_ToDial(long count, double percentage)
+			{
+				var dial = new CountThenPercentageDial(new CountDial(), new PercentageDial(rng));
+				Assert.IsTrue(dial.ToDial(count, percentage));
+				Assert.IsFalse(dial.ToDial(count, percentage));
+			}
+
+			[Test]
+			[TestCase(iterations / 2, 80d)] // 90%
+			[TestCase(iterations / 4, 20d)] // 40%
+			public void Sample_ToDial(long count, double percentage)
+			{
+				var dial = new CountThenPercentageDial(new CountDial(), new PercentageDial(rng));
+				var locker = new object();
+				int i = 0;
+				Parallel.For(0, iterations, loop =>
+				{
+					if (dial.ToDial(count, percentage))
+					{
+						lock (locker)
+						{
+							i++;
+						}
+					}
+				});
+				var dialedPercentage = (double)i / iterations * 100;
+				Console.WriteLine($"count: {count}, percentage: {percentage}, " +
+					$"i: {i}, dialedPercentage: {Math.Round(dialedPercentage, 4)}, iterations: {iterations}");
+			}
+
+			[Test]
+			[TestCase(0, 90d)]
+			[TestCase(10, 90d)]
+			[TestCase(1_000, 90d)]
+			[TestCase(iterations, 90d)]
+			public void Perf_ToDial(long count, double percentage)
+			{
+				var dial = new CountThenPercentageDial(new CountDial(), new PercentageDial(rng));
+				var stopwatch = Stopwatch.StartNew();
+				Parallel.For(0, iterations, loop =>
+				{
+					dial.ToDial(count, percentage);
+				});
+				stopwatch.Stop();
+				Console.WriteLine(stopwatch.ElapsedMilliseconds);
+			}
+		}
+	}
+}
