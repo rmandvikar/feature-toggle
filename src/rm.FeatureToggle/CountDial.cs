7d6b0ba67f6fcd97b3a97c8efd947cccbea65161
hippy <hippy2981@gmail.com>
2021-05-02 11:47:44 -0700

    ft count dial: Add impl

---
+++ src/rm.FeatureToggle/CountDial.cs
@@ -0,0 +1,30 @@
+﻿namespace rm.FeatureToggle
+{
+	/// <inheritdoc cref="ICountDial"/>
+	public class CountDial : ICountDial
+	{
+		private readonly object _lock = new object();
+		private long i;
+
+		/// <inheritdoc/>
+		public bool ToDial(long count)
+		{
+			if (count <= 0)
+			{
+				return false;
+			}
+			lock (_lock)
+			{
+				if (i >= count)
+				{
+					return false;
+				}
+				else // i < count
+				{
+					i++;
+					return true;
+				}
+			}
+		}
+	}
+}
---
+++ src/rm.FeatureToggle/ICountDial.cs
@@ -0,0 +1,13 @@
+﻿namespace rm.FeatureToggle
+{
+	/// <summary>
+	/// Defines dial methods with count.
+	/// </summary>
+	public interface ICountDial
+	{
+		/// <summary>
+		/// Returns true for first <paramref name="count"/> calls, and false after.
+		/// </summary>
+		bool ToDial(long count);
+	}
+}
