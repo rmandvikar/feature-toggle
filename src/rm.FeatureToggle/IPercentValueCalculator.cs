2961606dd75448bf7a8b3e28d98772bb24d94f4f
hippy <hippy2981@gmail.com>
2021-06-27 12:47:24 -0700

    ft guid dial: Pull out logic to calculate percent

---
+++ src/rm.FeatureToggle/IPercentValueCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace rm.FeatureToggle
+{
+	/// <summary>
+	/// Defines methods to deterministically calculate percent value.
+	/// </summary>
+	public interface IPercentValueCalculator
+	{
+		/// <summary>
+		/// Returns a deterministic value [0.01, 100.00] for given <paramref name="bytes"/>.
+		/// </summary>
+		double Calculate(byte[] bytes);
+	}
+}
---
+++ src/rm.FeatureToggle/PercentValueCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace rm.FeatureToggle
+{
+	/// <inheritdoc cref="IPercentValueCalculator"/>
+	public class PercentValueCalculator : IPercentValueCalculator
+	{
+		/// <inheritdoc/>
+		public double Calculate(byte[] bytes)
+		{
+			_ = bytes
+				?? throw new ArgumentNullException(nameof(bytes));
+
+			// 0.01-100.00
+			// note: GetHashCode()) seems like an ideal candidate
+			// but hashCode could be different across app domains, processes,
+			// platforms, .net implementations, etc
+			var n = new BigInteger(bytes);
+			n = n >= 0 ? n : -n;
+			return ((int)(n % 100_00) + 1) / (double)100;
+		}
+	}
+}
