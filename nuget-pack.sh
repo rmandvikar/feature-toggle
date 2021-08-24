ff2f3ed9f7c54a8aadb6dcb17fc7821b29fdd44e
hippy <hippy2981@gmail.com>
2021-08-24 00:47:03 -0700

    nuget: Add version, build metadata to assembly

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -18,7 +18,7 @@ metadata="$commit_hash"
 dotnet pack src/rm.FeatureToggle/rm.FeatureToggle.csproj \
 	-c Release \
 	-o .nupkg/ \
-	//p:Version="$version" \
+	//p:Version="$version+$metadata" \
 	//p:PackageVersion="$version+$metadata" \
 	//p:PackageReleaseNotes="tag: $tag" \
 	&& git tag "$tag" -m "Create nuget tag $tag"
