c8c85929a9ed42bbc216cb6ac39d25ee863413a4
hippy <hippy2981@gmail.com>
2022-05-16 14:23:51 -0700

    ft id dial: Use xxHash to calculate percent logic

--- tests/rm.FeatureToggleTest/IdDialTests.cs
+++ tests/rm.FeatureToggleTest/IdDialTests.cs
@@ -16,8 +16,9 @@ namespace rm.FeatureToggleTest
 		public class ToDial
 		{
 			[Test]
-			[TestCase("ascii", 13.62d, true)]
-			[TestCase("\0\0", 0.01d, true)]
+			[TestCase("ascii", 98.73d, true)]
+			[TestCase("\0\0", 0.98d, true)]
+			[TestCase("", 0.0d, false)]
 			public void Verify_ToDial(string id, double percentage, bool toDial)
 			{
 				var dial = new IdDial();
