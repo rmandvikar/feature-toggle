7631f4e3e0e598a0995497a76d8db8d6780627e5
hippy <hippy2981@gmail.com>
2026-09-19 19:12:00 -0700

    Revert "nuget: Rename name to rmandvikar.*"
    This reverts commit 6540c9c61628eb4434e4fec79fb0eabe7ffc012f.

--- nuget-delete.sh
+++ nuget-delete.sh
@@ -11,7 +11,7 @@ if [[ -z "$version" ]]; then
 	exit 1
 fi
 
-dotnet nuget delete rmandvikar.FeatureToggle "$version" \
+dotnet nuget delete rm.FeatureToggle "$version" \
 	-k $(< ~/dump/.nuget.apikey) \
 	-s https://api.nuget.org/v3/index.json \
 	--non-interactive
